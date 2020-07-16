1faa0fc272e79c6c647a5c2b471ffa6cc69ff223
Sarita Peter <64759475+Saritapeter123@users.noreply.github.com>
2020-07-16 12:32:18 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 var weatherData = $(this).attr("data-name");
 var queryURL = "api.openweathermap.org/data/2.5/forecast?q={city name}" + weatherData + "&appid={tango}";
+
+// Ajax call for searching cities. 
+$.ajax({
+    url: queryURL,
+    method: "GET"
+    //callback
+}).then(function(response) {
+
+    // Creating div to hold the search.
+    var cityDiv = $("<div class='city'>");
+    // Storing search.
+    var history = response.history;
+    // Creating and element to display the search.
+    var liOne = $("<li>").text("history", + history);
+    // Display the searched city on glass.
+    cityDiv.append(liOne);
+});
