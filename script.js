dd0f090647f1dec88beb45c40d4d7b3bde147922
Sarita Peter <64759475+Saritapeter123@users.noreply.github.com>
2020-07-17 18:31:12 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-var weatherData = $(this).attr("data-name");
-var queryURL = "api.openweathermap.org/data/2.5/forecast?q={city name}" + weatherData + "&appid={tango}";
+$(document).ready()
 
-// Ajax call for searching cities. 
-$.ajax({
-    url: queryURL,
-    method: "GET"
+function buildingQueryURL() {
+    var weatherData = $(this).attr("data-name");
+    var queryURL = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}";
+    var queryParams = {"api-key": "77f9fbbb114dc063ba452cf5bcc8f3cf" };
+
+    // Ajax call for searching cities. 
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+
     //callback
-}).then(function(response) {
+    }).then(function(response) {
 
-    // Creating div to hold the search.
-    var cityDiv = $("<div class='city'>");
-    // Storing search.
-    var history = response.history;
-    // Creating and element to display the search.
-    var liOne = $("<li>").text("history", + history);
-    // Display the searched city on glass.
-    cityDiv.append(liOne);
-});
+        // Creating div to hold the search.
+        var cityDiv = $("<div class='city'>");
+        // Storing search.
+        var history = response.history;
+        // Creating and element to display the search.
+        var liOne = $("<li>").text("history", + history);
+        // Display the searched city on glass.
+        cityDiv.append(liOne);
+    });
+};
