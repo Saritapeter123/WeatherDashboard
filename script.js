ab596a6f0707fe0e3a08ba3cf64ad0009d5bf1f0
Sarita Peter <64759475+Saritapeter123@users.noreply.github.com>
2020-07-22 20:39:03 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready()
 
-function buildingQueryURL() {
+function citySearching() {
     var weatherData = $(this).attr("data-name");
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q={city name}" + + "&appid=77f9fbbb114dc063ba452cf5bcc8f3cf";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q={city name}" + city + "&appid=77f9fbbb114dc063ba452cf5bcc8f3cf";
     
 
     // Ajax call for searching cities. 
@@ -13,13 +13,13 @@ function buildingQueryURL() {
     //callback
     }).then(function(response) {
 
-        // Creating div to hold the search.
-        var cityDiv = $("<div class='city'>");
-        // Storing search.
-        var history = response.history;
-        // Creating and element to display the search.
-        var liOne = $("<li>").text("history", + history);
-        // Display the searched city on glass.
-        cityDiv.append(liOne);
+        console.log(response);
+
+       var cityName = $("<h1>").text(response.name);
+       var date = $("<h2>").text(response.date);
+       var iconImage = $("<img>").attr(response.iconImage);
+       var humidity = $("<h3>").text(response.humidity);
+       var windSpeed = $("<h3>").text(response.windSpeed);
+       var uvIndex = $("<img>").attr(response.uvIndex)
     });
 };
